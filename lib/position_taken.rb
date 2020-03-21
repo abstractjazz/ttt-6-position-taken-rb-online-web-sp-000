0a95611f8749f3d60340a51bb5bee986cdc805c0
Evan Burton <evan@heyevan.com>
2020-03-21 03:19:25 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,2 +1,6 @@
 def position_taken?(board,index) # code your #position_taken? method here!
+  if array[index_number] == " "
+    returns false
+  elsif array [index number] == index
+    returns true  
 end
